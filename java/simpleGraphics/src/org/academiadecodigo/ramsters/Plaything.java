6ea2b317a76c9b4b484059a625eccdc17d24925a
ojaoc <correia.jafonso@gmail.com>
2020-02-27 14:52:56 +0000

    Simple Graphics play arround

---
+++ java/simpleGraphics/src/org/academiadecodigo/ramsters/Plaything.java
@@ -0,0 +1,102 @@
+package org.academiadecodigo.ramsters;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+public class Plaything implements KeyboardHandler {
+
+    public static final int SIDE_BOARD = 600;
+
+    public static final int PADDING = 10;
+
+    private Keyboard keyboard;
+
+    private Rectangle rectangle;
+
+    public Plaything() {
+
+        this.keyboard = new Keyboard(this);
+
+        this.rectangle = new Rectangle(PADDING, PADDING, 20, 20);
+
+    }
+
+    public void init() {
+
+        rectangle.setColor(Color.BLACK);
+        rectangle.fill();
+
+        KeyboardEvent upPressed = new KeyboardEvent();
+        KeyboardEvent downPressed = new KeyboardEvent();
+        KeyboardEvent rightPressed = new KeyboardEvent();
+        KeyboardEvent leftPressed = new KeyboardEvent();
+
+        upPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        upPressed.setKey(KeyboardEvent.KEY_UP);
+
+        downPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        downPressed.setKey(KeyboardEvent.KEY_DOWN);
+
+        rightPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        rightPressed.setKey(KeyboardEvent.KEY_RIGHT);
+
+        leftPressed.setKeyboardEventType(KeyboardEventType.KEY_PRESSED);
+        leftPressed.setKey(KeyboardEvent.KEY_LEFT);
+
+        keyboard.addEventListener(upPressed);
+        keyboard.addEventListener(downPressed);
+        keyboard.addEventListener(rightPressed);
+        keyboard.addEventListener(leftPressed);
+
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+
+        switch (keyboardEvent.getKey()) {
+
+            case KeyboardEvent.KEY_UP:
+
+                if (rectangle.getY() - PADDING < PADDING) return;
+
+                rectangle.translate(0, -10);
+
+                break;
+
+            case KeyboardEvent.KEY_DOWN:
+
+                if (rectangle.getY() + PADDING >= SIDE_BOARD) return;
+
+                rectangle.translate(0, 10);
+
+                break;
+
+            case KeyboardEvent.KEY_RIGHT:
+
+                if (rectangle.getX() + PADDING >= SIDE_BOARD) return;
+
+                rectangle.translate(10, 0);
+
+                break;
+
+            case KeyboardEvent.KEY_LEFT:
+
+                if (rectangle.getX() - PADDING < PADDING) return;
+
+                rectangle.translate(-10, 0);
+
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
+}
--- java/simpleGraphics/src/org/academiadecodigo/ramsters/Sandbox.java
+++ java/simpleGraphics/src/org/academiadecodigo/ramsters/Sandbox.java
@@ -6,25 +6,23 @@ import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
-public class Sandbox implements KeyboardHandler {
+public class Sandbox {
 
     public static void main(String[] args) {
 
-        Rectangle rectange = new Rectangle(0, 0, 400, 400);
+        Plaything movable = new Plaything();
+
+        Rectangle rectange = new Rectangle(Plaything.PADDING, Plaything.PADDING, Plaything.SIDE_BOARD, Plaything.SIDE_BOARD);
 
         rectange.setColor(Color.GREEN);
 
         rectange.fill();
 
-        Rectangle smallRectangle = new Rectangle(0, 0, 10, 10);
+        movable.init();
 
-        smallRectangle.setColor(Color.BLACK);
 
-        smallRectangle.fill();
 
-        Keyboard keyboard = new Keyboard(this);
 
-        KeyboardEvent event = new KeyboardEvent();
 
 
 
